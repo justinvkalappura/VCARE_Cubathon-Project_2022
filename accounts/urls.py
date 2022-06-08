960fb374be66ff3819aa0024f89c414e2c4a8a0f
Justin V Kalappura <93908687+justinvkalapura@users.noreply.github.com>
2022-06-08 14:27:37 +0530

    Add files via upload

---
+++ accounts/urls.py
@@ -0,0 +1,13 @@
+
+from django.urls import path
+from django.urls.conf import include
+from . import views
+
+
+urlpatterns = [
+    path('login',views.login,name='login'),
+    path('signup',views.signup,name='signup'),
+    path('logout',views.logout,name='logout'),
+
+
+]
